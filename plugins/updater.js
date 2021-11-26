3d64ffac6e6918295c55d8307f70debfb27a6988
kavishka <88529228+lkruwan@users.noreply.github.com>
2021-11-26 10:30:51 +0530

    ERROR FIX

--- plugins/updater.js
+++ plugins/updater.js
@@ -21,6 +21,62 @@ const Lang = Language.getString('updater');
 
 
 
+
+
+
+
+
+/* Copyright (C) 2020 Yusuf Usta.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WhatsAsena - Yusuf Usta
+*/
+
+const simpleGit = require('simple-git');
+const git = simpleGit();
+const MyPnky = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
+const exec = require('child_process').exec;
+const Heroku = require('heroku-client');
+const { PassThrough } = require('stream');
+const heroku = new Heroku({ token: Config.HEROKU.API_KEY })
+
+const Language = require('../language');
+const Lang = Language.getString('updater');
+
+
+
+MyPnky.addCommand({pattern: 'haveup$', fromMe: true, dontAddCommandList: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
+    await git.fetch();
+    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+    if (commits.total === 0) {
+        await message.client.sendMessage(
+            message.jid,
+            Lang.UPDATE, MessageType.text
+        );    
+    } else {
+        commits['all'].map(
+            (commit) => {
+                newzel= '🔹 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' <' + commit.author_name + '>\n';
+            }
+        );
+        
+        var webimage = await axios.get(`https://api.zeks.me/api/nulis?apikey=LUSIFARUSERbtokbro&text=lusifarupdates`, { responseType: 'arraybuffer' })
+
+        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg  , caption: newzel })
+    
+      
+    }
+}));
+
+
+
+
+
+
+
+
 MyPnky.addCommand({pattern: 'update$', fromMe: true, dontAddCommandList: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
     await git.fetch();
     var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
@@ -44,7 +100,7 @@ MyPnky.addCommand({pattern: 'update$', fromMe: true, dontAddCommandList: true, d
     }
 }));
 
-
+/*
 MyPnky.addCommand({pattern: 'updts$', fromMe: true, dontAddCommandList: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
     await git.fetch();
     var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
@@ -63,7 +119,7 @@ MyPnky.addCommand({pattern: 'updts$', fromMe: true, dontAddCommandList: true, de
     }
 }));
 
-
+*/
 
 MyPnky.addCommand({pattern: 'update now$', fromMe: true,dontAddCommandList: true, desc: Lang.UPDATE_NOW_DESC}, (async (message, match) => {
     await git.fetch();
