406f360cd28568a561d61cbd6b7c970b37173335
kavishka <88529228+lkruwan@users.noreply.github.com>
2021-11-26 11:25:22 +0530

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -24,8 +24,6 @@ const axios = require('axios');
 
 
 
-//dasdadasdas
-
 /* Copyright (C) 2020 Yusuf Usta.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
